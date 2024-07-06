90adeca75b8f0794e1c6d2238f89792c9b8b78e8
Alexis Labanino <naviuseless@gmail.com>
2024-07-06 17:44:50 -0500

    first commit

---
+++ ProjectTest.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ProjectTest.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ProjectTest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectTest.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
