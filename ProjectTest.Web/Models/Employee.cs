d5b38434cc57c7ae395020871c76a242fe8ad5c7
Alexis Labanino <naviuseless@gmail.com>
2024-07-07 17:18:04 -0500

    Add Test and Refactor Code

--- ProjectTest.Web/Models/Employee.cs
+++ ProjectTest.Web/Models/Employee.cs
@@ -2,6 +2,17 @@ namespace ProjectTest.Web.Models
 {
     public class Employee
     {
+        public Employee(string fullName, string employeeID, decimal sin, decimal phoneNumber, string city, decimal salary, string country)
+        {
+            FullName = fullName;
+            EmployeeID = employeeID;
+            SIN = sin;
+            PhoneNumber = phoneNumber;
+            City = city;
+            Salary = salary;
+            Country = country;
+        }
+
         public string FullName { get; set; } = string.Empty;
         public string EmployeeID { get; set; } = string.Empty;
         public decimal SIN { get; set; }
